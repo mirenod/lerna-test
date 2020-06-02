b8fc6452d4268746d25723dd04e3cd8d19c38043
Mireno <mirenod@hotmail.com>
2020-06-02 12:37:49 +0100

    fix(components): :art: adding some code

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
 
-const Button = (): ReactElement => <button type="submit">Mireno Button</button>;
+const Button = (): ReactElement => <button type="submit">Mireno2 Button</button>;
 
 export default Button;
